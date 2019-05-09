9ab4faed1a2bf74736697d873c354e6e63543b49
Antti Härkönen <antth@hotmail.fi>
2019-05-09 21:03:38 +0300

    Update bibliography.py

--- src/bibliography.py
+++ src/bibliography.py
@@ -1,26 +1,22 @@
 from collections import OrderedDict
+from operator import attrgetter
 
-from src.entry import Entry
+
+def handle_dict(entries):
+    pass
 
 
 class Bibliography:
     def __init__(self, entries: dict):
-        self._entries = {k, Entry(v) for k, v in entries.items()}
+        self.entries_dict = {k, handle_dict(v) for k, v in entries.items()}
         self._set_letters()
 
     @property
-    def entries_dict(self):
-        return {k: e.data for k, e in self._entries.items()}
-
-    @property
     def entries(self):
-        return [e.data for e in self._entries.values()]
+        return [e for e in self.entries_dict.values()]
 
     def _set_letters(self):
-        self._entries = sorted(lambda e: e['authors'][0][0], e['year'])
-        for k, e in self._entries.items():
-            if e.data.equals(last_e.data):
-
+        pass
 
 if __name__ == '__main__':
     pass
